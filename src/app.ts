de7f873c20420fb2623be169a6710bdeb17153ab
Victor J. Salazar <vagol942@gmail.com>
2018-03-23 18:30:41 -0500

    Implements the tip chat bar for donations, for this we added rxjs, because what else?

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import * as SocketIOClient from 'socket.io-client';
 import { Graphics } from 'phaser-ce';
+import * as Rx from 'rxjs';
 
 const game = new Phaser.Game(1280, 720, Phaser.AUTO);
 
@@ -90,6 +91,12 @@ class canvasState extends Phaser.State {
     canChangeText: Phaser.Text;
     positionText: Phaser.Text;
 
+    tipMessageText: Phaser.Text;
+    dummyMessage: Phaser.Text;
+    tipMessageTextTween: Phaser.Tween;
+
+    tipMessageDataObserver: Rx.Subject<any>;
+
     init(gridData, socket) {
         this.canvas = new GameCanvas(this.canvas_WIDTH, this.canvas_HEIGHT);
         this.canvas.populateGrid(gridData);
@@ -108,7 +115,6 @@ class canvasState extends Phaser.State {
 
         this.paintGrid(graphics);
         
-        /*
         graphics.beginFill(0x283149)
         graphics.drawRect(240,0, game.world.width - 240, 80);
         graphics.endFill();
@@ -116,11 +122,12 @@ class canvasState extends Phaser.State {
         graphics.beginFill(0x000000)
         graphics.drawRect(0,0, 240, 80);
         graphics.endFill();
-        */
 
+        /*
         graphics.beginFill(0x000000)
         graphics.drawRect(0,0, 1280, 80);
         graphics.endFill();
+        */
 
         const style3 = {
             fill: "#FFF",
@@ -139,20 +146,34 @@ class canvasState extends Phaser.State {
             fill: "#FFF",
             font: "14px Press Start 2P",
         }
+
+        const style7 = {
+            fill: "#FFF",
+            font: "20px Press Start 2P"
+        }
+
+        this.dummyMessage = this.add.text(-1000, -1000, "DUMMY", style7);
+        this.tipMessageText = this.add.text(1280, 30, "[None, $10]: This is not a true ending", style7);
+        this.tipMessageTextTween = this.add.tween(this.tipMessageText);
+
+        const timeBackground = this.add.graphics(0, 0);
+        timeBackground.beginFill(0);
+        timeBackground.drawRect(0, 0, 240, 80);
+        timeBackground.endFill();
         
-        this.timeText = this.add.text(640, 39, Date.now().toString(), style3);
+        this.timeText = this.add.text(125, 39, Date.now().toString(), style3);
         this.timeText.pivot.x = this.timeText.width/2;
         
-        // const hText = this.add.text(40, 68, "H", style4);
-        const hText = this.add.text(40+520, 68, "H", style4);
-        // const mText = this.add.text(120, 68, "M", style4);
-        const mText = this.add.text(120+520, 68, "M", style4);
-        // const sText = this.add.text(195, 68, "S", style4);
-        const sText = this.add.text(195+520, 68, "S", style4);
+        const hText = this.add.text(40, 68, "H", style4);
+        // const hText = this.add.text(40+520, 68, "H", style4);
+        const mText = this.add.text(120, 68, "M", style4);
+        // const mText = this.add.text(120+520, 68, "M", style4);
+        const sText = this.add.text(195, 68, "S", style4);
+        // const sText = this.add.text(195+520, 68, "S", style4);
 
-        // const timeRemainingText = this.add.text(23, 10, "TIME REMAINING", style5);
-        const timeRemainingText = this.add.text(1280/2, 10, "TIME REMAINING", style5);
-        timeRemainingText.pivot.x = timeRemainingText.width/2;
+        const timeRemainingText = this.add.text(23, 10, "TIME REMAINING", style5);
+        // const timeRemainingText = this.add.text(1280/2, 10, "TIME REMAINING", style5);
+        // timeRemainingText.pivot.x = timeRemainingText.width/2;
 
         // Set up the popUp on hover thing
         this.popUpGraphics = game.add.graphics(-100, -100);
@@ -163,6 +184,7 @@ class canvasState extends Phaser.State {
         this.colorText = game.add.text(-100, -100, "", style6);
         this.canChangeText = game.add.text(-100, -100, "", style6);
         this.positionText = game.add.text(-100, -100, "", style6);
+
 
         this.postCreate(graphics);
     }
@@ -187,7 +209,7 @@ class canvasState extends Phaser.State {
         this.canvas.grid.forEach(col => col.forEach( dot => this.paintDot(graphics, dot)));
     }
     paintDot(graphics, dot: GameDot, exalted = false) {
-        console.log('dot painted!!');
+        // console.log('dot painted!!');
         if (!exalted) {
             graphics.beginFill(dot.color);
             graphics.drawRect(dot.x*10, dot.y*10+80, 10, 10);
@@ -225,6 +247,20 @@ class canvasState extends Phaser.State {
         this.canChangeText.position = new Phaser.Point((dot.x*10) + 2 + xDisp, (dot.y*10)+80-60 + 42);
         this.canChangeText.text = dot.canChange ? `This pixel can be changed!`: `This pixel CANNOT be changed!`;
     }
+    calculateMessageDuration(messageData) {
+        this.dummyMessage.text = `[${messageData.username}, $${parseInt(messageData.amount)}]: ${messageData.message}$`;
+        return (this.dummyMessage.width + 1280 + 1280 - 240)/0.15
+    }
+    runMessage(messageData) {
+        if (!messageData.dummy) {
+            console.log("Moving Message")
+            this.tipMessageText.x = 1280;
+            this.tipMessageText.text = `[${messageData.username}, $${parseInt(messageData.amount)}]: ${messageData.message}`;
+            const tween = this.add.tween(this.tipMessageText);
+            tween.to({x: 240 - this.tipMessageText.width}, this.calculateMessageDuration(messageData));
+            tween.start();
+        }
+    }
 
     postCreate(graphics) {
         const self = this;
@@ -240,23 +276,57 @@ class canvasState extends Phaser.State {
             }
         });
 
+        const dummyMessageData = {
+            dummy: true
+        } 
+
+        this.tipMessageDataObserver = new Rx.Subject();
+        const streamObserver = this.tipMessageDataObserver
+                                .concatMap(messageData => Rx.Observable.from([messageData]).concat(Rx.Observable.from([dummyMessageData]).delay(self.calculateMessageDuration(messageData)+100)))
+        streamObserver.subscribe(messageData => self.runMessage(messageData));
+
+        this.socket.on('tipMessage', messageData => {
+            console.log(messageData);
+            // truncate if messag is too long
+            if (messageData.message && messageData.message.length > 200) {
+                messageData.message = messageData.message.substring(0, 199);
+            }
+            self.tipMessageDataObserver.next(messageData);
+        })
+        /*
+        setTimeout(() => {
+            console.log(`New message received!`)
+            this.tipMessageDataObserver.next({message: "hello dkashdkjashd aksjhd akjsdhakjsa"})
+        }, 1000);
+        setTimeout(() => {
+            console.log(`New message received!`)
+            this.tipMessageDataObserver.next({message: "hello2 aksdhaskdhas kdjahkdh askdh"})
+        }, 3000);
+        setTimeout(() => {
+            console.log(`New message received!!`)
+            this.tipMessageDataObserver.next({message: "Heellllo 33"})
+        }, 3500);
+        */
         window.addEventListener("mousemove", e => {
             const hoverX = Math.floor(game.input.x/10);
             const hoverY = Math.floor((game.input.y - 80)/10);
-            // If the hoveredDot is not defined, we created
-            if (!this.hoveredDot) {
-                this.hoveredDot = this.canvas.grid[hoverX][hoverY];
-                this.paintDot(graphics, this.hoveredDot, true)
-                this.createPopUp(this.hoveredDot);
-            }
-            // If the hoveredDot is different to the current hovering dot, change the color of the hoveredDot and change it to the new one
-            else if (this.hoveredDot !== this.canvas.grid[hoverX][hoverY]) {
-                // reset color of the hovered dot
-                this.paintDot(graphics, this.hoveredDot);
-                // set the new hovered dot and paint it
-                this.hoveredDot = this.canvas.grid[hoverX][hoverY];
-                this.paintDot(graphics, this.hoveredDot, true)
-                this.createPopUp(this.hoveredDot);
+            // Check if hoverY >= 0, that is, if the mouse is over the canvas
+            if (hoverY >= 0) {
+                // If the hoveredDot is not defined, we created
+                if (!this.hoveredDot) {
+                    this.hoveredDot = this.canvas.grid[hoverX][hoverY];
+                    this.paintDot(graphics, this.hoveredDot, true)
+                    this.createPopUp(this.hoveredDot);
+                }
+                // If the hoveredDot is different to the current hovering dot, change the color of the hoveredDot and change it to the new one
+                else if (this.hoveredDot !== this.canvas.grid[hoverX][hoverY]) {
+                    // reset color of the hovered dot
+                    this.paintDot(graphics, this.hoveredDot);
+                    // set the new hovered dot and paint it
+                    this.hoveredDot = this.canvas.grid[hoverX][hoverY];
+                    this.paintDot(graphics, this.hoveredDot, true)
+                    this.createPopUp(this.hoveredDot);
+                }
             }
         })
     }
